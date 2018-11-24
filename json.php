d2b1098047731b940b56f8b7de5ab17be9d93118
zenall <45296597+zenall@users.noreply.github.com>
2018-11-24 08:34:43 +0700

    Update json.php

--- json.php
+++ json.php
@@ -4,7 +4,7 @@
 	include "curl_gd.php";
 	$url = isset($_GET['url']) ? htmlspecialchars($_GET['url']) : null;
 	if(empty($url)) {
-	  $url = 'https://drive.google.com/file/d/0ByaRd0R0Qyatcmw2dVhQS0NDU0U/view';
+	  $url = 'https://drive.google.com/file/d/1VHco1KxapTm40mJ1_PTq5brfU1DHX6aF/view';
 	}
 	$gid = get_drive_id($url);
 	$results = GoogleDrive($gid);
