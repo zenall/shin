057b649659ef1e301fa6e362b2c606a132d2a580
zenall <45296597+zenall@users.noreply.github.com>
2018-11-27 21:07:49 +0700

    Update embed.php

--- embed.php
+++ embed.php
@@ -27,13 +27,48 @@
 	<title><?php echo $results['title'];?></title>
 	<!-- Plyr.io Player -->
 	<link rel="stylesheet" href="https://cdn.plyr.io/3.3.12/plyr.css">
+		<style type="text/css">
+		video {
+		  background-color: black;
+		  background-image: /* our video */;
+		  background-position: center center;
+		  background-size: contain;
+		}
+		#video-bg {
+		  position: fixed;
+		  top: 0; right: 0; bottom: 0; left: 0;
+		  overflow: hidden;
+		}
+		#video-bg > video {
+		  position: absolute;
+		  top: 0;
+		  left: 0;
+		  width: 100%;
+		  height: 100%;
+		}
+		/* 1. No object-fit support: */
+		@media (min-aspect-ratio: 16/9) {
+		  #video-bg > video { height: 300%; top: -100%; }
+		}
+		@media (max-aspect-ratio: 16/9) {
+		  #video-bg > video { width: 300%; left: -100%; }
+		}
+		/* 2. If supporting object-fit, overriding (1): */
+		@supports (object-fit: cover) {
+		  #video-bg > video {
+		    top: 0; left: 0;
+		    width: 100%; height: 100%;
+		    object-fit: cover;
+		  }
+		}
+	</style>
 </head>
 <body style="margin:0px;">
-
+	<div id="video-bg">
 	<video poster="<?php echo $results['image']; ?>" id="player" playsinline controls>
 		<source src="<?php echo $results['file'];?>" type="<?php echo $results['type'];?>">
 	</video>
-
+	</div>
 	<!-- Plyr JS -->
 	<script src="https://cdn.plyr.io/3.3.12/plyr.js"></script>
 	<script>const player = new Plyr('#player');</script>
